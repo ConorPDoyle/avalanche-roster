5e4fbe3916df663a94bdab2b3b9045beb3dc7785
conor <doyle.conorp@gmail.com>
2022-10-10 13:48:21 -0700

    Testing out class components & callback

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      name: { firstName: "Conor", lastName: "Doyle" },
+      company: "Kennedy Catholic",
+    };
   }
 
   render() {
@@ -13,8 +18,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi Conor.</p>
-          <button>Change Name</button>
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}. I work at{" "}
+            {this.state.company}, which is okay I guess.
+          </p>
+          <button
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  name: { firstName: "Zog", lastName: "Wog" }
+                }
+              }, 
+              () => {
+                console.log(this.state);
+              });
+              // console.log(this.state);
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
